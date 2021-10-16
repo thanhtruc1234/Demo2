6abd7a3aad75df7d40e92eedd5355f893d7def85
thanhtruc1234 <buithanhtruc12a8@gmail.com>
2021-10-16 16:19:45 +0700

    Them tac gia va nam vao file HelloGit

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,7 @@
 public class HelloGit {
 public static void main (String [] args) {
 System.out.println("welcome to version control System with Git");
+System.out.println("Author : Linus Torvald");
+System.out.println("Year : 2005");
 }
 }
